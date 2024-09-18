1e7bead45a863f239745015ed5c6bbb5a6d2df2e
liuli1 <liuli1@360.cn>
2024-09-18 18:14:55 +0800

    json val code add example tag

--- rsp/rsp.go
+++ rsp/rsp.go
@@ -2,7 +2,7 @@ package rsp
 
 // JSONVal http response common data.
 type JSONVal struct {
-	Code int         `json:"errno"`
+	Code int         `json:"errno" example:"0"`
 	Msg  string      `json:"msg"`
 	Data interface{} `json:"data"`
 }
